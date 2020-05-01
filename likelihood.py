30a47f9fd75fb57e48158bb1abb78f1d29998c1f
Patrick Wells <pwells@ucdavis.edu>
2020-05-01 10:22:57 -0700

    added docstrings to likelihood

--- likelihood.py
+++ likelihood.py
@@ -12,17 +12,49 @@ NEED_PARAMS = ['Omega_m', 'Omega_lambda', 'H0']
 NEED_NUISANCE = ['M_nuisance']
 
 class LK:
-    """
-    likelihood class
-    -----------
-    usage example:
-    -----------
-    import likelihood
-    LK = likelihood.LK()
-    log_likelihood = LK.likelihood_cal(par = [])
-    """
 
     def __init__(self, dat_dir=os.getcwd() + '/Binned_data/'):
+
+        """
+        likelihood class
+
+
+        Params
+        -----------
+        data_dir: location where pantheon data is found
+
+        Attributes
+        ----------
+
+        data: dict
+        A dictionary containing all the data from the dataset
+
+        stat_err: [floats]
+        List containing all the statistical error read in from the dataset
+
+        sys_err: [floats]
+        List containing all the systematic errors read in from the datset
+
+        m_B: [floats]
+        List containing apparent magnitudes read in from the dataset
+
+        z: [floats]
+        List containing redshifts read in from the dataset
+
+        tot_err: [floats]
+        List containing total error (sum of stat_err and sys_err)
+
+        usage example:
+        -----------
+        import likelihood
+        like = likelihood.LK()
+        pars = {'Omega_m': 0.30, 'Omega_lambda': 0.7, 'H0': 72.0,
+                'M_nuisance': -19.0, 'Omega_k': 0.0}
+        log_likelihood, parans = like.likelihood_cal(pars)
+        """
+
+
+
         self.data = self.loading_data(dat_dir)
         self.stat_err = self.data['stat_err']
         self.sys_err = self.data['sys_err']
@@ -96,40 +128,35 @@ class LK:
 
         Parameters:
         -----------
-        data : dictionary;
-        input the observation data, i.e., stat_err, sys_err, z, m_B
-
-        par : dictionary;
-        parameters from sampler
+        par : dictionary {string: float};
+        dictionary of parameters and their values
 
         ifsys: bool;
         calculate likelihood with and without systematic error
 
         Returns:
         --------
-        likelihood : float;
-        return the log-likelihood to the sampler
+        likelihood, pars : float, dict;
+        return the log-likelihood to the sampler, as well as parameters
         """
 
-        """
-        below is a test, we need to work on the code to calculate delta_mu from par
-        """
         print(pars.keys())
-        for param in NEED_PARAMS:
-            assert param in pars.keys(), 'Error: likelihood calculation requires a value'\
-                                         ' for  parameter {}'.format(param)
-        
-        for param in NEED_NUISANCE:
-            assert param in pars.keys(), 'Error: Likelihood requires nuisance parameter {}'.format(param)
+        for param in NEED_PARAMS: #check for all neede params
+            assert param in pars.keys(), 'Error: likelihood calculation'\
+                                         ' requires a value for parameter {}'.format(param)
 
-        for k,v in pars.items():
+        for param in NEED_NUISANCE: #check for all needed nuisance parameters
+            assert param in pars.keys(), 'Error: Likelihood requires nuisance'\
+                                          ' parameter {}'.format(param)
+
+        for k,v in pars.items(): #check that value are valid
             assert isinstance(v, float), 'Error: value of paramater {} is not a float'.format(k)
 
         model_mus = self.compute_model(pars) + pars.get('M_nuisance')
         delta_mu = self.m_B - model_mus
         delta_mu = np.matrix(delta_mu)
-        
-        if(ifsys):
+
+        if(ifsys): #loads the error
             error = self.tot_err
         else:
             error = self.stat_err
@@ -137,37 +164,76 @@ class LK:
         #Claculate Chi2 according to Equation 8
         Chi2 = np.float(delta_mu * np.linalg.inv(error) * np.transpose(delta_mu))
 
-        #temporary output for test, will be deleted
         return -Chi2/2, pars #returns the log-likelihood
-        
+
 
     def compute_model(self, pars):
         '''
-        Computes the model values  given a set of parameters. Note, the validity of the input parameters
-        is checked by the calling function
+        Computes the model values  given a set of parameters.
+        The validity of the input parameters is
+        checked by the calling function (likelihood_cal)
 
+        Parameters:
+        -----------
+        par : dictionary {string: float};
+        dictionary of parameters and their values
+
+        Returns:
+        ------------
+        mus: [float]
+        Values of the distance moduli for the given model
         '''
-        if not 'Omega_k' in pars.keys():
+
+        if not 'Omega_k' in pars.keys(): #calculates omegak, if needed
             omega_k = 1 - pars.get('Omega_lambda') - pars.get('Omega_m')
             if np.abs(omega_k) < (10**-7):
                 omega_k = 0 # compensating for floating point arithmetic errors
             pars.update({'Omega_k': omega_k})
 
         lds = self.luminosity_distances(pars)
-        mus = np.log10(100000*lds) # luminosity distances are in units of megaparsecs
+        mus = 5 + np.log10(lds)
+        #luminosity distances are in units of megaparsecs
         return mus
 
 
 
-    def integrand(self, z, params):
+    def integrand(self, z, pars):
+        '''
+        Returns the value of the integrand E(z) that is used in all luminosity
+        distance calculations
+
+        Parameters:
+        -----------
+        z: float
+        redshift value
+
+        pars: dict {string: float}
+        Values of cosmological parameters
+
+        Returns:
+        --------
+        The value of the integrand for the given redshift and cosmological
+        parameters
+
+        '''
+
         assert z > 0, 'Error: Invalid value for redshift passed. z must be > 0'
-        sum = params['Omega_m']*((1+z)**3) + params['Omega_lambda'] + params['Omega_k']*((1+z)**2)
+        sum = pars['Omega_m']*((1+z)**3) + pars['Omega_lambda'] + pars['Omega_k']*((1+z)**2)
         return 1/np.sqrt(sum)
 
     def luminosity_distances(self, pars):
         '''
         Calculates the luminosity distances for a given model, in units of mpc
 
+        Parameters:
+        -----------
+        pars: dict {string: float}
+        Values of cosmological parameters
+
+        Returns:
+        --------
+        lds: [floats]
+        Calculated luminosity distances in units of megaparsecs
         '''
         num_points = len(self.z)
         lds = np.zeros(num_points)
@@ -179,10 +245,31 @@ class LK:
         for i in range(1, num_points):
             integral_val += quad(self.integrand, self.z[i-1], self.z[i], args=(pars,))[0]
             lds[i] = self.luminosity_delegate(self.z[i], Omega_k, integral_val)
-
+            # Here we avoid excess calulation by integrating over redshift
+            # ranges and summing
         return lds
 
     def luminosity_delegate(self, z, Omega_k, integral_val):
+        '''
+        Helper function for calculating the luminosity distnaces
+
+        Parameters:
+        -----------
+        z: float
+        Redshift value
+
+        Omega_k: float
+        Curvature density
+
+        integral_val: float
+        value of the integral E(z) for the given redshift
+
+        Returns:
+        --------
+        luminosity distance: float
+        Calculated luminosity distances in units of megaparsecs
+        '''
+
         d_hubble = self.hubble_distance(pars.get('H0'))
         if Omega_k > 0:
             return (1+z)*d_hubble*np.sinh(np.sqrt(Omega_k)*integral_val)/np.sqrt(Omega_k)
@@ -194,16 +281,28 @@ class LK:
 
 
     def hubble_distance(self, H0):
+        '''
+        Calculates the hubble distance in unites of mpc for a given H0
+
+        Parameters:
+        -----------
+        H0: float
+        Value of the hubble constant
+
+        Returns:
+        -----------
+        hubble distance: float
+        value of the hubble distance
+        '''
+
         c = 3*10**5
-        Hubble_m = H0
-        return c/Hubble_m
+        return c/H0
 
 
 
 if __name__ == '__main__':
     import matplotlib.pyplot as plt
     lk = LK()
-    pars = {'Omega_m': 0.30, 'Omega_lambda': 0.7, 'H0': 72.0, 'M_nuisance': 19.0}
-    chi2, pars = lk.likelihood_cal(pars, False)
+    pars = {'Omega_m': 0.3001, 'Omega_lambda': 0.6999, 'H0': 72.0, 'M_nuisance': 19.0, 'Omega_k': 0.0}
+    chi2, pars = lk.likelihood_cal(pars)
     print(chi2)
-    print(pars)
